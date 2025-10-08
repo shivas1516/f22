42477804bf497984dca4d0c583de3c939cc6738d
SIVASUBRAMANIAM L <141582345+shivas1516@users.noreply.github.com>
2025-10-08 13:55:06 +0530

    OpenAI to HuggingFace

--- wishper_poc/wishper_file.py
+++ wishper_poc/wishper_file.py
@@ -1,27 +1,47 @@
 import gradio as gr
-from openai import OpenAI
-import os
-from dotenv import load_dotenv
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+import time
+import GPUtil
 
-load_dotenv()
-openai_api_key = os.getenv("OPENAI_API_KEY")
+# Device setup
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+print("Loading model...")
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id,
+    dtype=dtype,
+    low_cpu_mem_usage=True,  # Reduce RAM usage
+    device_map="auto" if torch.cuda.is_available() else None,
+    use_safetensors=True,
+)
+processor = AutoProcessor.from_pretrained(model_id)
+
+# pipeline
+asr_pipe = pipeline("automatic-speech-recognition", model=model, tokenizer=processor.tokenizer, feature_extractor=processor.feature_extractor)
 
 
-client = OpenAI(api_key=openai_api_key)
+def transcribe_audio(audio_file):
+    start_time = time.time()
+    result = asr_pipe(audio_file)
+    latency = time.time() - start_time
+    if torch.cuda.is_available():
+        gpus = GPUtil.getGPUs()
+        gpu_info = "\n".join([f"GPU {gpu.id}: {gpu.memoryUsed}/{gpu.memoryTotal} MB used, {gpu.load*100:.1f}% load" for gpu in gpus])
+    else:
+        gpu_info = "CPU mode, no GPU available."
 
-def from_file(audio_file):
-    with open(audio_file, "rb") as f:
-        transcription = client.audio.transcriptions.create(
-            model="whisper-large-v3",
-            file=f
-        )
-    return transcription.text
+    print(f"Transcription latency: {latency:.2f} seconds")
+    print(gpu_info)
+
+    return result["text"]
+
 
 file_demo = gr.Interface(
-    fn=from_file,
-    inputs=gr.Audio(sources=["upload"], type="filepath"),
-    outputs="text",
-    title="Whisper Demo - File Input"
+    fn=transcribe_audio, inputs=gr.Audio(sources=["upload"], type="filepath"), outputs="text", title="Optimized Whisper Transformers Demo - File Input"
 )
 
 file_demo.launch()
